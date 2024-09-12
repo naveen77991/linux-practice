d7170b9addbba1cfcf44e395e1a9cacaa2426f1f
na <naveen765>
2024-09-12 13:13:19 +0530

    shell

--- array.sh
+++ array.sh
@@ -1,14 +1,2 @@
 #! /bin/bash
-source="/home/ec2-user/logs"
-LOGFILE="/home/ec2-user/logs/h1"
-mkdir -p  h1
-
-
-if [ -d $source ]
-then
-   echo "exists"  >> "$LOGFILE"
-else
-    echo "statement2"
-fi
-
-echo "hi naveen" >> $LOGFILE
+echo "hi"
